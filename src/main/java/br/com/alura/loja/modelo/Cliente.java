f1b88dec87657105c64f36ad31d860356c775049
Raphael Vacari <raphaelvacari7@gmail.com>
2023-09-28 13:39:51 -0300

    Mapeando novas entidades

---
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -0,0 +1,47 @@
+package br.com.alura.loja.modelo;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String nome;
+    private String cpf;
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Cliente() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
